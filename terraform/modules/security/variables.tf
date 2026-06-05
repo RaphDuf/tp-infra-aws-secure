ac8e9a6ca02a01ad8aa350674e740fbf4512532c
Axel Malka <axelmalka@outlook.fr>
2026-06-05 10:31:15 +0200

    fix syntaxe for var definition

--- terraform/modules/security/variables.tf
+++ terraform/modules/security/variables.tf
@@ -1,5 +1,21 @@
-variable "project"            { type = string }
-variable "vpc_id"             { type = string }
-variable "admin_ip"           { type = string  default = "0.0.0.0/0" }
-variable "private_subnet_ids" { type = list(string) }
-variable "public_subnet_cidr" { type = string  default = "10.0.1.0/24" }s
+variable "project" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "admin_ip" {
+  type        = string
+  description = "Votre IP publique (sans /32)"
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
